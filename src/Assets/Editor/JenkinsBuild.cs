d9ea28ae549797dfedc83d839666aab2bdc397cd
git-yulia <julia.maliauka953@myci.csuci.edu>
2021-03-15 18:45:40 -0700

    Imported and corrected the JenkinsBuild file. Time to test it.

--- src/Assets/Editor/JenkinsBuild.cs
+++ src/Assets/Editor/JenkinsBuild.cs
@@ -5,40 +5,42 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using UnityEditor.Build.Reporting;
- 
+
 // ------------------------------------------------------------------------
-// https://docs.unity3d.com/Manual/CommandLineArguments.html
+// https://docs.unity3d.com/ScriptReference/
 // ------------------------------------------------------------------------
 public class JenkinsBuild 
 {
-    static string[] EnabledScenes = FindEnabledEditorScenes();
     static string APP_NAME = "src";
     static string TARGET_DIR = "C:\\Jenkins\\BUILDS";
 
     public static void BuildOnWindows()
     {
         string target_dir = APP_NAME + ".app";
-        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
+        GenericBuild(TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 
-    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    static void GenericBuild(string target_dir, BuildTarget build_target, BuildOptions build_options)
     {
-        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
-        if (res.Length > 0) 
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/ChemClub.unity" };
+        buildPlayerOptions.locationPathName = "generic_build";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        // EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+
+        BuildReport build_report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = build_report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
         {
-            throw new Exception("BuildPlayer failure: " + res);
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
         }
     }
-
-    private static string[] FindEnabledEditorScenes() 
-    {
-		List<string> EditorScenes = new List<string>();
-		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) 
-        {
-			if (!scene.enabled) continue;
-			EditorScenes.Add(scene.path);
-		}
-		return EditorScenes.ToArray();
-	}
 }
