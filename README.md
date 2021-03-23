d0f67ce53620e70d068d25aaba574b8df4274458
git-yulia <julia.maliauka953@myci.csuci.edu>
2021-03-22 18:47:25 -0700

    Added SetUp function to EditModeTests so the scene is opened prior to running tests.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # HoloTest
-Creating an automated test framework for HoloLens 2 using Unity Test Framework and Perception Simulation. Also includes fun Unity tinkering!
+
+## Assumptions
+- The Jenkinsfile assumes the source code is hosted on GitHub. This can be configured in the Checkout stage. 
+
+## Preflight Checklist
+- [Jenkins] Install required plugins: 
+1. Credentials Plugin
+2. Git Plugin
+3. GitHub Plugin
+4. All of the Pipeline-related plugins. (including Declarative)
+5. Generic Webhook Trigger Plugin
+6. Build With Parameters
+7. xUnit
+8. Workspace Cleanup
+
+I've also included a full list of plugins installed on my server (in this repo's automation directory). 
+There are a lot of Jenkins plugins, and many do similar things, so in retrospect it is hard to identify
+which ones are actually being utilized. 
+
+- [Jenkinsfile] Check the Unity installations against those listed in the Environment section.
+- [Jenkinsfile] Change the Git URL in the Checkout stage to match the new source repository. 
+- [Jenkins Settings] Add user credentials, either as a direct account, or with SSH and a private key. 
+- [Jenkinsfile] Change credentialsId field to match the one you set in Jenkins. 
+- [Jenkins Settings] Set a custom workspace - some directory on the build machine.
--- src/Assets/Tests/ChemClub/EditMode/EditModeTests.cs
+++ src/Assets/Tests/ChemClub/EditMode/EditModeTests.cs
@@ -11,57 +11,82 @@ using Microsoft.MixedReality.Toolkit.Utilities.Editor;
 
 namespace HoloTest_Namespace
 {
-    // You can also organize tests with the following attribute:
-    // [UnityPlatform (RuntimePlatform.WindowsPlayer)]
-
     public class EditModeTests
     {
-        [Test]
-        public void CheckSceneLoadsCorrectly()
+        /// <summary>
+        /// The SetUp function runs before any of the tests do. 
+        /// </summary>
+        [SetUp]
+        public void SetUp()
         {
-            var scene = EditorSceneManager.OpenScene("Assets/Scenes/ChemClub.unity");
-            Assert.IsTrue(scene.IsValid());
+            // Open the test scene used in this demo.
+            string sceneName = "Assets/Scenes/ChemClub.unity";
+            Scene scene = EditorSceneManager.OpenScene(sceneName);
         }
 
+        /// <summary>
+        /// Example that shows how you can open a specific scene 
+        /// in the Editor.
+        /// </summary>
         [Test]
-        public void InvalidSceneDoesNotLoad()
+        public void CheckScenesOpenCorrectly()
         {
-            Scene scene = new Scene(); 
+            string sceneName = "Assets/Scenes/ChemClub.unity";
+            Scene scene = EditorSceneManager.OpenScene(sceneName);
+            Assert.IsTrue(scene.IsValid());
 
+            // As an example, you can check the inverse - that a 
+            // nonexistent scene does NOT open. 
+            Scene fakeScene = new Scene();
             try
             {
-                scene = EditorSceneManager.OpenScene("Assets/Scenes/ThisSceneDoesntExist.unity");
+                fakeScene = EditorSceneManager.OpenScene("Assets/Scenes/ThisSceneDoesntExist.unity");
             }
             catch
             {
-                Debug.Log("Could not open this scene, since it doesn't exist! (And that's good.)");
+                Debug.Log("Could not open nonexistent scene! (And that's good.)");
             }
             finally
             {
-                Assert.IsFalse(scene.IsValid());
+                Assert.IsFalse(fakeScene.IsValid());
             }
+
+            // I open the swap scene here to check that SetUp is 
+            // indeed running before everything else does. 
+            Scene swapScene = EditorSceneManager.OpenScene("Assets/Scenes/SwapScene.unity");
+            Assert.IsTrue(swapScene.IsValid());
         }
 
+        /// <summary>
+        /// This examples shows how you could check that the initial 
+        /// size of an object is correct. 
+        /// </summary>
         [Test]
-        public void CheckBurnerSize()
+        public void CheckBurnerHeight()
         {
-            // Test that the camera has not been moved from the origin point. 
-            EditorSceneManager.OpenScene("Assets/Scenes/ChemClub.unity");
+            // First, check that the game object exists at all 
             GameObject burner = GameObject.Find("bunsen_burner");
             Assert.IsNotNull(burner);
 
+            // You can also use other components, such as Colliders.
             Renderer renderer = burner.GetComponent<Renderer>();
+
+            // Transform.localScale corresponds to the scale values
+            // visible inside the inspector. 
             float burnerHeight = renderer.transform.localScale.y;
-            Assert.GreaterOrEqual(burnerHeight, 0.1);
+            Assert.GreaterOrEqual(burnerHeight, 0.4);
         }
 
+        /// <summary>
+        /// This example checks that the camera has not been moved
+        /// from the origin point, since this would upset MRTK.
+        /// </summary>
         [Test]
         public void CheckInitialCameraSettings()
         {
-            // Test that the camera has not been moved from the origin point. 
-            EditorSceneManager.OpenScene("Assets/Scenes/ChemClub.unity");
             GameObject go_MainCamera = GameObject.Find("main_camera");
             Assert.IsNotNull(go_MainCamera);
+
             Vector3 camera_position = go_MainCamera.transform.position;
             Vector3 expected_position = new Vector3(0.0f, 0.0f, 0.0f);
             Assert.AreEqual(expected_position, camera_position);
